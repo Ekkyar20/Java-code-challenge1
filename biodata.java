54cb4e52f1a3892e511c0b575f80a56260220f33
ekky arya said <Ekkysaid20@gmail.com>
2021-10-12 13:03:00 +0700

    first commit

---
+++ biodata.java
@@ -0,0 +1,18 @@
+public class biodata{
+    public static void main(String[] args){ 
+        String firstName = "Ekky ";
+        String lastName  = "Arya Said ";
+        int age = 21;
+        String addres = "Eka Surya";
+        String phoneNumber = "0895365724797 ";
+        String hobby = "futsal ";
+        String makananKesukaan = "Kentang Goreng ";
+        System.out.println("Nama : " + firstName + lastName);
+        System.out.println("age : " + age);
+        System.out.println("Addres : " + addres);
+        System.out.println("phone number : " + phoneNumber);
+        System.out.println("hobby : " + hobby);
+        System.out.println("makanan kesukaan : " + makananKesukaan);
+
+    }
+}
